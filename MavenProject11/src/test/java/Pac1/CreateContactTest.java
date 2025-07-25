1285f7d6d4bec319803a50e91d3abca674383003
kavita-shah <https://github.com/kavita-shah/VtigerAppFw.git>
2025-07-25 11:16:30 +0530

    first push

---
+++ MavenProject11/src/test/java/Pac1/CreateContactTest.java
@@ -0,0 +1,27 @@
+package Pac1;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	
+	@Test
+	public void createConatctTest1() {
+		String URL=System.getProperty("url");
+		String BROWSER = System.getProperty("browser", "Chrome");
+		String USERNAME = System.getProperty("browser");
+		String PASSWORD = System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
+		System.out.println("execute CreateContactTest");
+	}
+	
+	@Test
+	public void modifyContactTest() {
+		System.out.println("execute modifyCretaeContactTest");
+	}
+
+}
---
+++ MavenProject11/src/test/java/Pac2/CreateOrgTest.java
@@ -0,0 +1,16 @@
+package Pac2;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+@Test
+public void createOrgTest() {
+	
+	System.out.println("execute orgTest");
+}
+
+@Test
+public void modifyOrgTest() {
+	System.out.println("execute modifyOrgTest");
+}
+}
